ad9c55dbd377c32ebde5af96b238cda740b2ae46
Matt Eason <github@matteason.co.uk>
2025-03-17 23:16:38 +0000

    Update opengraph properties

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,18 @@ export default defineNuxtConfig({
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1, viewport-fit=cover' },
         { name: 'theme-colour', content: '#0b0c0c' },
-        { property: 'og:image', content: 'https://tf230.matteason.co.uk/images/og-image.png' }
+        { property: 'og:image', content: 'https://tf230.matteason.co.uk/images/og-image.png' },
+        {
+          property: 'og:description',
+          content:
+            'Use this service to claim financial reimbursement for a tooth which has been lost and cannot be collected by the Tooth Fairy'
+        },
+        {
+          property: 'description',
+          content:
+            'Use this service to claim financial reimbursement for a tooth which has been lost and cannot be collected by the Tooth Fairy'
+        },
+        { property: 'twitter:card', content: 'summary_large_image' }
       ],
       link: [{ rel: 'icon', sizes: 'any', href: '/images/favicon.svg', type: 'image/svg+xml' }]
     }
